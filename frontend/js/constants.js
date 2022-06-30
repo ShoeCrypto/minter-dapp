1ce686583cdcea1e39aa9c52274fabdcec75b036
ShoeCrypto <werner.bundschuh@hotmail.com>
2022-06-30 19:52:56 +0200

    text

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,8 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Edgy Gainers NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
+const welcome_p = 'The Edgy Gainers NFT Project is a decentralized, open-source project that aims to share its gains with the community. For more info check out our discord and get part of our community';
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
